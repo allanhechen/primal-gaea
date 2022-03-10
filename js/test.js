50354c77714f82e5cb1dcde611a33cfc2b138fb1
Allan <alanhe159@gmail.com>
2022-03-10 18:56:36 -0500

    add slider

---
+++ js/test.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  autoplay: {
+    delay: 5000,
+  },
+});
