ba47b3efe4f4272e34c35a3dae3eadd0c34a253c
Allan <alanhe159@gmail.com>
2022-03-12 21:05:20 -0500

    try to fix loading times

--- js/slide.js
+++ js/slide.js
@@ -2,3 +2,11 @@ function slide() {
   var element = document.getElementById('nav-menu');
   element.classList.toggle("visible")
 }
+
+document.addEventListener('click', function(e){
+  x = e.pageX
+  element = document.getElementById("nav-menu");
+  if (element.classList.contains("visible") && $(window).width() - x > 350) {
+    element.classList.toggle("visible");
+  }
+});
