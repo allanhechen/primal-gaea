ea4cc60455942f9a8c1c92f2519e95b2b5c73279
Allan <alanhe159@gmail.com>
2022-03-16 21:13:11 -0400

    done dropdown menu

--- js/slide.js
+++ js/slide.js
@@ -1,24 +1,29 @@
+var element = document.getElementById("nav-menu");
+var navBar = document.getElementById("navbar");
+var dropDown = document.getElementById("dropdown-menu");
+const height = window.innerHeight * 0.75;
+
 function slide() {
-  var element = document.getElementById('nav-menu');
   element.classList.toggle("visible")
 }
 
 document.addEventListener('click', function(e){
-  x = e.pageX
-  var element = document.getElementById("nav-menu");
+  x = e.pageX;
+  target = e.target;
+  if (dropDown.classList.contains("active") && target.id != "dropdown-menu" && target.id != "more") {
+    // console.log(target);
+    dropDown.classList.toggle("active");
+  }
   if (element.classList.contains("visible") && $(window).width() - x > 350) {
     element.classList.toggle("visible");
   }
 });
 
-var thing = document.getElementById("navbar");
-var dropDown = document.getElementById("dropdown-menu")
-
 var myScrollFunc = function() {
-  if (window.scrollY >= window.innerHeight) {
-    thing.classList.add("view");
+  if (window.scrollY >= height) {
+    navBar.classList.add("view");
   } else {
-    thing.classList.remove("view");
+    navBar.classList.remove("view");
     dropDown.classList.remove("active")
   }
 };
@@ -26,6 +31,5 @@ var myScrollFunc = function() {
 window.addEventListener("scroll", myScrollFunc);
 
 function dropdown() {
-  var element = document.getElementById("dropdown-menu");
-  element.classList.toggle("active")
+  dropDown.classList.toggle("active");
 }
