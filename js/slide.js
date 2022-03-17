f32e6f6d1ddfc1b38b27a252f069d5eb10d42dbd
Allan <alanhe159@gmail.com>
2022-03-16 20:56:03 -0400

    dropdown menu

--- js/slide.js
+++ js/slide.js
@@ -5,20 +5,27 @@ function slide() {
 
 document.addEventListener('click', function(e){
   x = e.pageX
-  element = document.getElementById("nav-menu");
+  var element = document.getElementById("nav-menu");
   if (element.classList.contains("visible") && $(window).width() - x > 350) {
     element.classList.toggle("visible");
   }
 });
 
 var thing = document.getElementById("navbar");
+var dropDown = document.getElementById("dropdown-menu")
 
 var myScrollFunc = function() {
   if (window.scrollY >= window.innerHeight) {
     thing.classList.add("view");
   } else {
     thing.classList.remove("view");
+    dropDown.classList.remove("active")
   }
 };
 
 window.addEventListener("scroll", myScrollFunc);
+
+function dropdown() {
+  var element = document.getElementById("dropdown-menu");
+  element.classList.toggle("active")
+}
