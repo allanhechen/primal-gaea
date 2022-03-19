75217ef0fd2cba440ab2259a30f7b985c52c18b0
Allan <alanhe159@gmail.com>
2022-03-19 10:30:59 -0400

    capitalize L in loading

--- js/preloader.js
+++ js/preloader.js
@@ -1,8 +1,10 @@
 preloader = document.getElementById("preloader");
-preloader.style.opacity = "0";
-sleep(500).then(() => {
-  preloader.remove();
-});
+window.onload = function () {
+  preloader.style.opacity = "0";
+  sleep(500).then(() => {
+    preloader.remove();
+  });  
+} 
 
 function sleep (time) {
   return new Promise((resolve) => setTimeout(resolve, time));
