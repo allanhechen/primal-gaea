ff1ec28fb43775808635d3ef64fc393815165039
Allan <alanhe159@gmail.com>
2022-03-21 13:11:43 -0400

    fix overlay + navbar interaction

--- js/scroll.js
+++ js/scroll.js
@@ -2,9 +2,14 @@ const navBar = document.getElementById("navbar");
 // const dropDown = document.getElementById("dropdown-menu");
 const overlay = document.getElementById("overlay-links");
 const height = window.innerHeight * 0.5;
+const footer = document.getElementById("footer");
 
 var myScrollFunc = function() {
-  if (window.scrollY >= height) {
+  // console.log(footer.getBoundingClientRect().top < window.innerHeight);
+  if (footer.getBoundingClientRect().top < window.innerHeight) {
+    navBar.classList.add("view-navbar");
+    overlay.classList.remove("visible");
+  } else if (window.scrollY >= height) {
     navBar.classList.add("view-navbar");
     overlay.classList.add("visible");
   } else {
