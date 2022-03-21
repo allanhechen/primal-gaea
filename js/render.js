1ebbf3f50a77a628398eae376e4be9c9ea503df6
Allan <alanhe159@gmail.com>
2022-03-20 20:01:20 -0400

    fix nav font

--- js/render.js
+++ js/render.js
@@ -1,80 +1,98 @@
+'use strict';
 
-let scene, camera, renderer;
-
-function init() {
-
-  scene = new THREE.Scene();
-  scene.background = new THREE.Color(0xdddddd);
-
-  camera = new THREE.PerspectiveCamera(40,window.innerWidth/window.innerHeight,1,5000);
-  // camera.rotation.y = 45/180*Math.PI;
-  // camera.position.x = 800;
-  // camera.position.y = 100;
-  // camera.position.z = 1000;
-
-  // hlight = new THREE.AmbientLight (0x404040,100);
-  // scene.add(hlight);
-
-  // directionalLight = new THREE.DirectionalLight(0xffffff,100);
-  // directionalLight.position.set(0,1,0);
-  // directionalLight.castShadow = true;
-  // scene.add(directionalLight);
-  // light = new THREE.PointLight(0xc4c4c4,10);
-  // light.position.set(0,300,500);
-  // scene.add(light);
-  // light2 = new THREE.PointLight(0xc4c4c4,10);
-  // light2.position.set(500,100,0);
-  // scene.add(light2);
-  // light3 = new THREE.PointLight(0xc4c4c4,10);
-  // light3.position.set(0,100,-500);
-  // scene.add(light3);
-  // light4 = new THREE.PointLight(0xc4c4c4,10);
-  // light4.position.set(-500,300,500);
-  // scene.add(light4);
-
-  renderer = new THREE.WebGLRenderer({antialias:true});
-  renderer.setSize(window.innerWidth - 100,window.innerHeight - 100);
-  document.body.appendChild(renderer.domElement);
-
-  // let loader = new THREE.GLTFLoader();
-  // loader.load('../img/building', function(gltf){
-  //   model = gltf.scene.children[0];
-  //   // model.scale.set(0.5,0.5,0.5);
-  //   console.log(model)
-  //   scene.add(gltf.scene);
-  //   animate();
-  // });
+/* global THREE */
 
 
-  // instantiate a loader
-const loader = new THREE.OBJLoader();
+function main() {
+  const canvas = document.querySelector('#1');
+  const renderer = new THREE.WebGLRenderer({canvas});
 
-  // load a resource
-  loader.load(
-    // resource URL
-    '../img/building/B1/buildings1.obj',
-    // called when resource is loaded
-    function ( object ) {
-      scene.add( object );
-    },
-    // called when loading is in progresses
-    function ( xhr ) {
-      console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-    },
-    // called when loading has errors
-    function ( error ) {
-      console.log( 'An error happened' );
+  const fov = 45;
+  const aspect = 2;  // the canvas default
+  const near = 0.1;
+  const far = 100;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(0, 10, 20);
+
+  const controls = new THREE.OrbitControls(camera, canvas);
+  controls.target.set(0, 5, 0);
+  controls.update();
+
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color('black');
+
+  {
+    const planeSize = 40;
+
+    const loader = new THREE.TextureLoader();
+    const texture = loader.load('https://r105.threejsfundamentals.org/threejs/resources/images/checker.png');
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.magFilter = THREE.NearestFilter;
+    const repeats = planeSize / 2;
+    texture.repeat.set(repeats, repeats);
+
+    const planeGeo = new THREE.PlaneBufferGeometry(planeSize, planeSize);
+    const planeMat = new THREE.MeshPhongMaterial({
+      map: texture,
+      side: THREE.DoubleSide,
+    });
+    const mesh = new THREE.Mesh(planeGeo, planeMat);
+    mesh.rotation.x = Math.PI * -.5;
+    scene.add(mesh);
+  }
+
+  {
+    const skyColor = 0xB1E1FF;  // light blue
+    const groundColor = 0xB97A20;  // brownish orange
+    const intensity = 1;
+    const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+    scene.add(light);
+  }
+
+  {
+    const color = 0xFFFFFF;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(0, 10, 0);
+    light.target.position.set(-5, 0, 0);
+    scene.add(light);
+    scene.add(light.target);
+  }
+
+  {
+    const objLoader = new THREE.OBJLoader2();
+    objLoader.load('img/building/B1/buildings1.obj', (event) => {
+      const root = event.detail.loaderRootNode;
+      scene.add(root);
+    });
+  }
+
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
     }
-  );
+    return needResize;
+  }
 
+  function render() {
 
-  // let controls = new THREE.OrbitControls(camera, renderer.domElement);
-  // // let controls = new THREE.OrbitControls(camera, renderer.domElement);
-  // controls.addEventListener('change', renderer);
-  
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+
+  requestAnimationFrame(render);
 }
-function animate() {
-  renderer.render(scene,camera);
-  requestAnimationFrame(animate);
-}
-init();
+
+main();
