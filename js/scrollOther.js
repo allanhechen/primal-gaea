61b43faf2ec30f0695036e6b7a61c6992d09233a
Allan <alanhe159@gmail.com>
2022-03-28 17:14:35 -0400

    add overlay to whitepaper

---
+++ js/scrollOther.js
@@ -0,0 +1,15 @@
+const overlay = document.getElementById("overlay-links");
+const footer = document.getElementById("footer");
+
+var myScrollFunc = function() {
+  // console.log(footer.getBoundingClientRect().top < window.innerHeight);
+  if (footer.getBoundingClientRect().top < window.innerHeight) {
+    overlay.classList.remove("visible");
+  } else if (window.scrollY >= 0) {
+    overlay.classList.add("visible");
+  } else {
+    overlay.classList.remove("visible");
+  }
+};
+
+window.addEventListener("scroll", myScrollFunc);
