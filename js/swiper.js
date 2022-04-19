b0bbda1c420e5877ef7b11bacf4e5ca13df9270e
TempestIndra <43696183+TempestIndra@users.noreply.github.com>
2022-04-19 01:31:34 -0400

    little bug fix

--- js/swiper.js
+++ js/swiper.js
@@ -1,49 +1,48 @@
-(function() {
-    var swiper = new Swiper('.swiper-container', {
-        direction: 'horizontal',
-        loop: true,
-        simulateTouch : true,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-          },
+(function () {
+  var swiper = new Swiper(".swiper-container", {
+    direction: "horizontal",
+    loop: true,
+    simulateTouch: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
 
-         autoplay: {
-          delay: 5000,
-        },
-    });
-    
-    $('.swiper-container .swiper-slide').on('click', function() {
-      var slideId = $(this).attr('id');         
+    autoplay: {
+      delay: 5000,
+    },
+  });
+
+  $(".swiper-container .swiper-slide").on("click", function () {
+    var slideId = $(this).attr("id");
+    if ($(this).attr("id")) {
       openFullscreenSwiper(slideId);
-    });
-    
-    function openFullscreenSwiper(initialSlideNumber) {
-      var mainSwiperMarkup = $('.swiper-container').html(); 
-      
-      $('#fullscreen-swiper')
-        .append(mainSwiperMarkup + "<div id='fullscreen-swiper-close'>X</div>")
-        .fadeIn();
-      
-      var fullscreenSwiper = new Swiper('#fullscreen-swiper', {
-        pagination: '.swiper-pagination',
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        slidesPerView: 1,
-        centeredSlides: true,
-        loop: true,
-        initialSlide: initialSlideNumber - 1,
-      });      
-      
-      $('#fullscreen-swiper-backdrop').fadeIn();
-      $('body, html').addClass('no-scroll');  
-  
-      $('#fullscreen-swiper-close').on('click', function() {
-        $('#fullscreen-swiper').hide().empty();
-        $('#fullscreen-swiper-backdrop').fadeOut();
-        $('body, html').removeClass('no-scroll');
-      });
-    
     }
-  
-  })();
+  });
+
+  function openFullscreenSwiper(initialSlideNumber) {
+    var mainSwiperMarkup = $(".swiper-fullscreen").html();
+
+    $("#fullscreen-swiper")
+      .append(mainSwiperMarkup + "<div id='fullscreen-swiper-close'>X</div>")
+      .fadeIn();
+
+    var fullscreenSwiper = new Swiper("#fullscreen-swiper", {
+      nextButton: ".swiper-button-next",
+      prevButton: ".swiper-button-prev",
+      slidesPerView: 1,
+      centeredSlides: true,
+      loop: true,
+      initialSlide: initialSlideNumber - 1,
+    });
+
+    $("#fullscreen-swiper-backdrop").fadeIn();
+    $("body, html").addClass("no-scroll");
+
+    $("#fullscreen-swiper-close").on("click", function () {
+      $("#fullscreen-swiper").hide().empty();
+      $("#fullscreen-swiper-backdrop").fadeOut();
+      $("body, html").removeClass("no-scroll");
+    });
+  }
+})();
